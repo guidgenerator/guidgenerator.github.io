163e06f68dafafa8aa86fb87a12bf02aba757eda
Benjamin Hysell <ben.hysell@gmail.com>
2021-08-30 13:42:55 -0400

    new 6.0.0-preivew 7 with AOT

--- _content/BlazorStrap/blazorStrap.js
+++ _content/BlazorStrap/blazorStrap.js
@@ -27,7 +27,13 @@ window.blazorStrap = {
             if (!document.body.classList.contains("modal-open")) {
                 document.body.classList.add("modal-open");
             }
-            this.paddingRight("17px");
+            var body = document.body,
+                html = document.documentElement;
+            var height = Math.max(body.scrollHeight, body.offsetHeight,
+                html.clientHeight, html.scrollHeight, html.offsetHeight);
+            if (height > window.innerHeight) {
+                this.paddingRight("17px");
+            }
             return id;
         },
         close: function (id) {
@@ -192,9 +198,11 @@ window.blazorStrap = {
     },
 
     collapsingElementEnd: function (element) {
-        element.style.height = "";
-        element.classList.remove("collapsing");
-        element.classList.add("collapse");
+        if (element) {
+            element.style.height = "";
+            element.classList.remove("collapsing");
+            element.classList.add("collapse");
+        }
         return true;
     },
     setBootstrapCss: function (theme, version) {
@@ -212,11 +220,11 @@ window.blazorStrap = {
         return true;
     },
     setPopper: function () {
-        if (script === undefined) {
+        if (typeof Popper === undefined || script === undefined) {
             script = document.createElement('script');
             document.head.insertBefore(script, document.head.lastChild);
+            script.src = "_content/BlazorStrap/popper.min.js";
         }
-        script.src = "_content/BlazorStrap/popper.min.js";
         return true;
     }
 };
